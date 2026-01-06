882c360741d77ab916616ad7daad539c7d8e08b1
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-01-06 11:59:20 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-// supabase.js
-const SUPABASE_URL = 'https://hmyxiighjsyfgjcrsuil.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhteXhpaWdoanN5ZmdqY3JzdWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NzUyODEsImV4cCI6MjA3NzE1MTI4MX0.GxuPLLNQMbjcFlyFRHeJti6h6cwIQy_OU-n8i9XH-sk';
-
-// Exportar cliente de Supabase
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
